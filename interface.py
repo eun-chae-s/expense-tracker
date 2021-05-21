99b7cf36a6b158a444b1b1151be3ace2f0627ce7
eun-chae-s <s9710173@naver.com>
2021-05-22 00:50:00 +0900

    Update the total amount on the interface (but need to find a way to automatically update the price as soon as we add the price)

--- interface.py
+++ interface.py
@@ -4,11 +4,22 @@ This Python file is for creating the Desktop GUI of the expense tracker
 import tkinter as tk
 from datetime import datetime
 from daily import create_daily_interface
+import csv
 
 
 def open_daily() -> None:
     """This function opens a new window for recording the daily expense data."""
     create_daily_interface()
+
+
+def daily_expense() -> int:
+    """This function calculates the sum of all the expenses spent on that day."""
+    current_date = datetime.today().strftime('%Y-%m-%d')
+    with open('expense_record.csv') as c_file:
+        read_c_file = csv.reader(c_file)
+        next(read_c_file)
+
+        return sum([int(row[3]) for row in read_c_file if row[0] == current_date])
 
 
 app = tk.Tk()
@@ -22,12 +33,10 @@ date = tk.Label(app, text=datetime.today().strftime('%Y-%m-%d'),
                 font=("Helvetica", 13, 'italic')).place(relx=0.8, rely=0.01)
 
 # Overall record
-total_amount = tk.IntVar(app)
-total_amount.set(0)
 notification1 = tk.Label(app, text="Today, you spent ",
                          height=5,
                          font=("Helvetica", 20, 'bold')).place(relx=0.1, rely=0.05)
-amount_label = tk.Label(app, text='￦' + str(total_amount.get()),
+amount_label = tk.Label(app, text='￦' + str(daily_expense()),
                         height=4,
                         fg='pink',
                         font=("MS Sans Serif", 26, 'bold')).place(relx=0.4, rely=0.04)
